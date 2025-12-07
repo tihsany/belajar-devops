a68d72fa79304fb6ee002ca6b14a3ad5eb3cf5e2
tihsany <opikteea@gmail.com>
2025-12-07 07:50:55 +0000

    Menambahkan build dan test ke pipeline

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Mulai proses build..."
+sleep 2
+echo "Build selesai!"
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "🧪 Menjalankan testing..."
+sleep 2
+# Simulasi test berhasil
+echo "Semua test berhasil!"
+exit 0
