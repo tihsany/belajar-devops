08332413ac368a951ec1694456da64507d7f5a27
tihsany <opikteea@gmail.com>
2025-12-07 08:11:16 +0000

    Menambahkan test validasi biodata

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "🧪 Menjalankan testing..."
-sleep 2
-# Simulasi test berhasil
-echo "Semua test berhasil!"
-exit 0
+echo "Menjalankan test..."
+
+if [ -s biodata.txt ]; then
+  echo "Test berhasil: biodata.txt ditemukan dan tidak kosong."
+  exit 0
+else
+  echo "Test GAGAL: biodata.txt tidak ditemukan atau kosong!"
+  exit 1
+fi
