159f4de3ef1aca0538aede1872f8bbbb0cece76c
tihsany <opikteea@gmail.com>
2025-12-12 03:06:02 +0000

    Test new docker pipeline

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const PORT = 3001;
 app.get('/', (req, res) => {
 
   // Solusi: Gabungkan kedua pesan atau pilih satu yang benar
-  const gabunganPesan = 'Percobaan ke-2 update Latihan lagi CI/CD otomatisasi update dari fitur-biodata!';
+  const gabunganPesan = 'Percobaan ke-3 update Latihan lagi CI/CD meliputi build docker!';
   
   // Hanya SATU kali panggilan res.send()
   res.send(gabunganPesan); 
