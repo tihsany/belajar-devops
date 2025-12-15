a630cf6b1dab223c47978e70125c9b23bedf8892
tihsany <opikteea@gmail.com>
2025-12-15 00:12:22 +0000

    Update Host dan app

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ const PORT = 3001;
 app.get('/', (req, res) => {
 
   // Solusi: Gabungkan kedua pesan atau pilih satu yang benar
-  const gabunganPesan = 'Percobaan ke-3 update Latihan lagi CI/CD meliputi build docker!';
+  const gabunganPesan = 'Percobaan ke-4 update Latihan lagi CI/CD meliputi build docker!';
   
   // Hanya SATU kali panggilan res.send()
   res.send(gabunganPesan); 
