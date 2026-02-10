fe8ab2788402e944cf72d69d453b027b60cf6bea
tihsany <opikteea@gmail.com>
2026-02-10 09:26:26 +0000

    modif app dan host

--- app/app.js
+++ app/app.js
@@ -22,7 +22,7 @@ redisClient.on('error', (err) => console.error('Redis Error', err));
 app.get('/', async (req, res) => {
   const count = await redisClient.incr('visitor_count');
   res.json({
-    message: 'Hello from Docker + Redis 🚀 with opik',
+    message: 'Hello from Docker + Redis 🚀 to',
     visitor: count,
   });
 });
